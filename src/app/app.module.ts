77541a090a3080381707a1764a04344d97066632
Adel Acebal <adelacebal@yahoo.com>
2017-06-21 21:04:54 -0400

    image component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductsListComponent,
-    ProductRowComponent
+    ProductRowComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
+import { Product } from '../product/product.model';
 
 @Component({
   selector: 'product-row',
   templateUrl: './product-row.component.html',
   styleUrls: ['./product-row.component.css']
 })
-export class ProductRowComponent implements OnInit {
+export class ProductRowComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'item';
 
 }
